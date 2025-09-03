6463dbebffcb5e526b74df1560a133aaf9a107d0
SatyamShivamCDAC <satyamshivamcdac@gmail.com>
2025-09-03 09:01:16 +0300

    classwork uplaod

--- CoreJava/Assignment/src/day7/toyStore/Store.java
+++ CoreJava/Assignment/src/day7/toyStore/Store.java
@@ -4,6 +4,7 @@ package day7.toyStore;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -48,11 +49,9 @@ public class Store {
 //
 
 
-        Map<String,List<Toy>> listOfToys = stream.collect(Collectors.groupingBy(Toy::getAgeGroup));
+        Map<String,Long> listOfToys = stream.collect(Collectors.groupingBy(Toy::getAgeGroup,Collectors.counting()));
 
-        for(String key : listOfToys.keySet()){
-            System.out.println(key + " - " + listOfToys.get(key).size());
-        }
+        System.out.println(listOfToys);
 
 //        Map<String, Long> listOfToys = stream
 //                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
